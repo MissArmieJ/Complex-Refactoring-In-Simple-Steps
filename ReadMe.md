6041b37c7b0b016227864c77de9e8aafab7365e6
Armanda <adp@armanda.me.uk>
2017-06-01 13:30:30 +0100

    Transform Long Parameters code smell to immutable Package class

---
+++ ReadMe.md
@@ -0,0 +1,7 @@
+Followed Matthew Butt's screencasts on:  
+ 
+* https://codurance.com/videos/2016-09-02-complex-refactoring-in-
+simple-steps-I/
+* https://codurance.com/videos/2016-09-30-complex-refactoring-in-simple-steps-II/
+* https://codurance.com/videos/2017-01-30-complex-refactoring-in-simple-steps-III/
+
--- ReplaceMethodWithMethodObject/PostageCalculator/PostageCalculator/Calculator.cs
+++ ReplaceMethodWithMethodObject/PostageCalculator/PostageCalculator/Calculator.cs
@@ -2,25 +2,46 @@
 
 namespace PostageCalculator
 {
+    public class Package
+    {
+        private readonly int _weight;
+        private readonly int _height;
+        private readonly int _width;
+        private readonly int _depth;
+
+        public Package(int weight, int height, int width, int depth)
+        {
+            _weight = weight;
+            _height = height;
+            _width = width;
+            _depth = depth;
+        }
+
+        public int GetWeight() => _weight;
+        public int GetHeight() => _height;
+        public int GetWidth() => _width;
+        public int GetDepth() => _depth;
+    }
+
     public class Calculator
     {
         public Money Calculate(int weight, int height, int width, int depth, Currency currency)
         {
-            var postageInBaseCurrency = PostageInBaseCurrency(weight, height, width, depth);
+            var postageInBaseCurrency = PostageInBaseCurrency(new Package(weight, height, width, depth));
             return ConvertCurrency(postageInBaseCurrency, currency);
         }
 
-        private decimal PostageInBaseCurrency(int weight, int height, int width, int depth)
+        private decimal PostageInBaseCurrency(Package package)
         {
-            if (weight <= 60 && height <= 229 && width <= 162 && depth <= 25)
+            if (package.GetWeight() <= 60 && package.GetHeight() <= 229 && package.GetWidth() <= 162 && package.GetDepth() <= 25)
             {
                 return 120m;
             }
-            if (weight <= 500 && height <= 324 && width <= 229 && depth <= 100)
+            if (package.GetWeight() <= 500 && package.GetHeight() <= 324 && package.GetWidth() <= 229 && package.GetDepth() <= 100)
             {
-                return weight*4;
+                return package.GetWeight()*4;
             }
-            return Math.Max(weight, height*width*depth/1000m)*6;
+            return Math.Max(package.GetWeight(), package.GetHeight()*package.GetWidth()*package.GetDepth()/1000m)*6;
         }
 
         private Money ConvertCurrency(decimal amountInBaseCurrency, Currency currency)
