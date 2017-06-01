dfd5aedb6bc028517ef5bce97b97a439a89f833a
Armanda <adp@armanda.me.uk>
2017-06-01 13:57:39 +0100

    decompose & extract classes Small- Medium- LargePackage

--- ReplaceMethodWithMethodObject/PostageCalculator/PostageCalculator/Package.cs
+++ ReplaceMethodWithMethodObject/PostageCalculator/PostageCalculator/Package.cs
@@ -2,6 +2,42 @@
 
 namespace PostageCalculator
 {
+    public class MediumPackage
+    {
+        public MediumPackage(int weight)
+        {
+            Weight = weight;
+        }
+
+        public int Weight { get; private set; }
+
+        public int MediumPackagePostageInBaseCurrency()
+        {
+            return this.Weight * 4;
+        }
+    }
+
+    public class LargePackage
+    {
+        public LargePackage(int weight, int height, int width, int depth)
+        {
+            Weight = weight;
+            Height = height;
+            Width = width;
+            Depth = depth;
+        }
+
+        public int Weight { get; private set; }
+        public int Height { get; private set; }
+        public int Width { get; private set; }
+        public int Depth { get; private set; }
+
+        public decimal LargePackagePostageInBaseCurrency()
+        {
+            return Math.Max(this.Weight, this.Height*this.Width*this.Depth/1000m)*6;
+        }
+    }
+
     public class Package
     {
         private readonly int _weight;
@@ -19,15 +55,25 @@ namespace PostageCalculator
 
         public decimal PostageInBaseCurrency()
         {
-            if (_weight <= 60 && _height <= 229 && _width <= 162 && _depth <= 25)
+            if (IsSmall())
             {
-                return 120m;
+                return SmallPackage.SmallPackagePostageInBaseCurrency();
             }
-            if (_weight <= 500 && _height <= 324 && _width <= 229 && _depth <= 100)
+            if (IsMedium())
             {
-                return _weight*4;
+                return new MediumPackage(_weight).MediumPackagePostageInBaseCurrency();
             }
-            return Math.Max(_weight, _height*_width*_depth/1000m)*6;
+            return new LargePackage(_weight, _height, _width, _depth).LargePackagePostageInBaseCurrency();
+        }
+
+        private bool IsMedium()
+        {
+            return _weight <= 500 && _height <= 324 && _width <= 229 && _depth <= 100;
+        }
+
+        private bool IsSmall()
+        {
+            return _weight <= 60 && _height <= 229 && _width <= 162 && _depth <= 25;
         }
     }
 }
---
+++ ReplaceMethodWithMethodObject/PostageCalculator/PostageCalculator/SmallPackage.cs
@@ -0,0 +1,10 @@
+namespace PostageCalculator
+{
+    public class SmallPackage
+    {
+        public static decimal SmallPackagePostageInBaseCurrency()
+        {
+            return 120m;
+        }
+    }
+}
